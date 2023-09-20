ef164ba9b543bc217a725a275a39065b3dbfb37a
duardo10 <duardos36@gmail.com>
2023-09-20 11:41:47 -0300

    atual

--- pessoa.py
+++
@@ -1,9 +0,0 @@
-class Pessoa():
-    """
-    A classe pessoa é a classe Mae de paciente, os atributos utilizados foram cpf, nome, telefone e data de nascimento
-    """
-    def __init__(self, cpf, nome, telefone,dt_nasc):
-        self._cpf = cpf
-        self._nome = nome
-        self._telefone = telefone
-        self._telefone = dt_nasc
